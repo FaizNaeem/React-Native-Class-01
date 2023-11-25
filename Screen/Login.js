a06eb2203f04fdcc8edf530bf17cc5a2dedd1242
FaizNaeem <faiznaeem192022@gmail.com>
2023-11-25 18:25:08 +0500

    Login Complete

--- Screen/Login.js
+++ Screen/Login.js
@@ -1,35 +1,52 @@
-import { Button, StyleSheet, Text, TextInput, View ,TouchableOpacity, LinearGradient} from 'react-native';
+import axios from 'axios';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, TouchableOpacity, LinearGradient } from 'react-native';
 
-export default function Login({navigation}) {
-  const handler = () =>{
-alert("Login Suucess")
+export default function Login({ navigation }) {
+  let [email, setEmail] = useState("Random1@gmail.com")
+  let [password, setPassword] = useState("192022")
+  console.log('email-->', email);
+  console.log("password-->", password);
+  const login = {
+    email:email ,
+    password:password
+  }
+  // console.log(login);
+  const handler = () => {
+    axios.post("https://nice-blue-calf-gown.cyclic.app/user/Login",login)
+    alert("Login success")
+    navigation.goBack();
 
   }
   return (
 
     <View style={styles.container}>
-      <Text style={{fontSize:30, fontFamily:"serif"}}>Login</Text>
-         <TextInput
+      <Text style={{ fontSize: 30, fontFamily: "serif" }}>Login</Text>
+      <TextInput
         style={styles.input}
         placeholder='Enter Email'
+        onChangeText={(e) => setEmail(e)}
+        value={email}
         keyboardType='email-address'
-       
+
       />
-         <TextInput
+      <TextInput
         style={styles.input}
+        value={password}
+        onChangeText={(e) => setPassword(e)}
         placeholder='Enter Password'
         keyboardType='visible-password'
-       
+
       />
       {/* <Button
         title='Login'
         onPress={() => navigation.navigate('Home')}
       /> */}
-        <TouchableOpacity onPress={handler} style={{backgroundColor:"lightblue", width:300,alignItems:"center", marginTop:15,padding:10,borderRadius:5}}>
-  
-      <Text style={styles.appButtonText}>Login</Text>
- 
-  </TouchableOpacity>
+      <TouchableOpacity onPress={handler} style={{ backgroundColor: "lightblue", width: 300, alignItems: "center", marginTop: 15, padding: 10, borderRadius: 5 }}>
+
+        <Text style={styles.appButtonText}>Login</Text>
+
+      </TouchableOpacity>
 
     </View>
   );
@@ -40,23 +57,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    
+
     justifyContent: 'center',
   },
-  input :{
-  // width:200,
-   backgroundColor:"white",
-   padding:10,
-   borderColor:"gray",
-   borderStyle:"solid",
-   borderWidth:1,
-   borderRadius:5,
-   width:350,
-   marginTop:10
-  
+  input: {
+    // width:200,
+    backgroundColor: "white",
+    padding: 10,
+    borderColor: "gray",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 5,
+    width: 350,
+    marginTop: 10
+
   },
-  appButtonText:{
-    fontSize:20
+  appButtonText: {
+    fontSize: 20
   }
 
 
